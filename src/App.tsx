44df351e9b713698bcf5b62b170fa0a6939acf80
Vitor Diniz <vitor.diniz.dev@gmail.com>
2024-12-20 17:17:35 -0300

    Criação rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import { GameCard } from "./components/GameCard/GameCard";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { GameDetails } from "./pages/GameDetails";
+import TextField from "@mui/material/TextField";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex justify-between items-center p-3 bg-red-700 px-20">
+        <h1>Game Rating</h1>
+        <TextField
+          id="outlined-basic"
+          type="search"
+          variant="outlined"
+          size="small"
+        />
+      </div>
+    </header>
+  );
+};
 
 function App() {
   return (
     <>
-      <GameCard />
+      <Header />
+      <div className="p-20">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/game/:id" element={<GameDetails />} />
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
