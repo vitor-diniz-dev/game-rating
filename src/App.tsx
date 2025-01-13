10ba0bbe6b4e7a8593645aa72bc8a6bfabb6b436
Vitor Diniz <vitor.diniz.dev@gmail.com>
2025-01-13 11:58:44 -0300

    Adição rota Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router";
 import { Home } from "./pages/Home";
 import { GameDetails } from "./pages/GameDetails";
 import TextField from "@mui/material/TextField";
@@ -6,8 +6,11 @@ import TextField from "@mui/material/TextField";
 const Header = () => {
   return (
     <header>
-      <div className="flex justify-between items-center p-3 bg-red-700 px-20">
-        <h1>Game Rating</h1>
+      <div className="flex justify-between items-center py-4 bg-red-700 px-20">
+        <h1 className="text-4xl">Game Rating</h1>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
         <TextField
           id="outlined-basic"
           type="search"
@@ -22,15 +25,15 @@ const Header = () => {
 function App() {
   return (
     <>
-      <Header />
-      <div className="p-20">
-        <Router>
+      <Router>
+        <Header />
+        <div className="p-20">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/game/:id" element={<GameDetails />} />
           </Routes>
-        </Router>
-      </div>
+        </div>
+      </Router>
     </>
   );
 }
