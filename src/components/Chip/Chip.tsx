09afdda3cd395166e0703988cbac314d56dbcd36
Vitor Diniz <vitor.diniz.dev@gmail.com>
2025-01-14 15:27:35 -0300

    Ajustes visuais componente Chip

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,5 +1,5 @@
 import { ChipProps, Chip as MuiChip } from "@mui/material";
 
 export const Chip = ({ ...props }: ChipProps) => (
-  <MuiChip className="m-0.5" variant="outlined" {...props} />
+  <MuiChip variant="outlined" {...props} />
 );
