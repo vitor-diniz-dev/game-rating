0a3fc01c91d2187f0b354aa81b141f0f09a051ba
Vitor Diniz <vitor.diniz.dev@gmail.com>
2025-01-14 15:30:20 -0300

    Listagem jogos página Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { GameCard } from "../components/GameCard/GameCard";
-import { api } from "../api";
+import { Game, getHypedGames, getRecentGames } from "../services/gameService";
+import { Skeleton } from "@mui/material";
 
 export const Home = () => {
+  const [hypedGames, setHypedGames] = useState<Game[]>([]);
+  const [recentGames, setRecentGames] = useState<Game[]>([]);
+
   const loadGames = () => {
-    api.post("games").then((response) => {
-      console.log(response.data);
+    getHypedGames().then((games) => {
+      setHypedGames(games);
+    });
+    getRecentGames().then((games) => {
+      setRecentGames(games);
     });
   };
 
@@ -15,7 +22,42 @@ export const Home = () => {
 
   return (
     <>
-      <GameCard />
+      <section>
+        <h2 className="text-2xl mb-4">Hyped Games</h2>
+        {!hypedGames.length ? (
+          <Skeleton variant="rectangular" width="100%" height={340} />
+        ) : (
+          <div className="flex gap-2">
+            {hypedGames.map((game) => (
+              <GameCard
+                key={game.id}
+                id={game.id}
+                name={game.name}
+                rating={game.rating}
+                cover={game.cover}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+      <section>
+        <h2 className="text-2xl mb-4">Recent Games</h2>
+        {!recentGames.length ? (
+          <Skeleton variant="rectangular" width="100%" height={340} />
+        ) : (
+          <div className="flex gap-2">
+            {recentGames.map((game) => (
+              <GameCard
+                key={game.id}
+                id={game.id}
+                name={game.name}
+                rating={game.rating}
+                cover={game.cover}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     </>
   );
 };
