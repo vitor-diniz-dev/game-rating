2acd2629096d177f790e9854c29519b2cdebb57c
Vitor Diniz <vitor.diniz.dev@gmail.com>
2025-01-14 15:25:27 -0300

    Alteração cabeçalho default requisições

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ import { IGDBAPI } from "../configs/environment";
 export const api = axios.create({
   baseURL: IGDBAPI.url,
   headers: {
+    "Content-Type": "text/plain",
     "x-api-key": IGDBAPI.apiKey,
   },
 });
