1bd235792e36a461e9a51f376522dfc078b62458
Vitor Diniz <vitor.diniz.dev@gmail.com>
2025-01-14 15:28:25 -0300

    Alteração ícone componente Rating

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,28 +1,11 @@
-import { styled } from "@mui/material/styles";
 import MuiRating, { RatingProps } from "@mui/material/Rating";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-
-const StyledRating = styled(MuiRating)({
-  "& .MuiRating-iconFilled": {
-    color: "#ff4953",
-  },
-  "& .MuiRating-iconHover": {
-    color: "#ff3d47",
-  },
-});
 
 export const Rating = ({ ...props }: RatingProps) => {
   return (
-    <StyledRating
-      name="customized-color"
-      defaultValue={4}
-      getLabelText={(value: number) =>
-        `${value} Heart${value !== 1 ? "s" : ""}`
-      }
+    <MuiRating
+      name="game-rating"
+      getLabelText={(value: number) => `${value} Star${value !== 1 ? "s" : ""}`}
       precision={0.5}
-      icon={<FavoriteIcon fontSize="inherit" />}
-      emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
       {...props}
     />
   );
